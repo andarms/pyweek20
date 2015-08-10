1a0e989ec1361d3d42d2db35bb2b5b77cfd53441
Adrian <andarms@outlook.com>
2015-08-10 00:49:11 -0500

    Add camera viewport for the level

--- data/states/game/level.py
+++ data/states/game/level.py
@@ -3,52 +3,42 @@ import pygame as pg
 
 from ... import util, actors
 
-WORLD = [
-    "####################################################",
-    "#      #                                           #",
-    "#                                                  #",
-    "#                  ###########    ###########      #",
-    "#      #                                           #",
-    "#      #                                           #",
-    "#      #                           #######         #",
-    "#      #                                           #",
-    "#      #                                           #",
-    "#      #                            #######        #",
-    "#      #                                           #",
-    "#      #                                   ####### #",
-    "#                                                  #",
-    "#                                                  #",
-    "#            #################                     #",
-    "#                                                  #",
-    "#                                                  #",
-    "#                                                  #",
-    "#    ###################      ######### ############",
-    "#                                                  #",
-    "#                                                  #",
-    "####################################################"
-]
-WALL_SIZE = 32
+def clear_callback(surface, rect):
+    """
+    We need this callback because the clearing background contains
+    transparency.  We need to fill the rect with transparency first.
+    """
+    surface.fill((0,0,0,0), rect)
+
 class Level(object):
     """docstring for Level"""
-    def __init__(self):     
-        self.max_enemies = 7
+    def __init__(self):
+        w = len(util.WORLD[0])*util.WALL_SIZE
+        h = len(util.WORLD)*util.WALL_SIZE
+        self.image = pg.Surface((w,h))
+        self.rect = self.image.get_rect()
+        self.background = pg.Surface(self.image.get_size())
+        self.background = self.background.convert()
+        self.background.fill((0,0,0))
+        self.max_enemies = 17
         self.all_sprites = pg.sprite.LayeredDirty()
         self.walls = self.make_walls()
         self.enemies = self.make_enemies()
         self.player_singleton = pg.sprite.GroupSingle()
         self.player = actors.Player([50,50], self.player_singleton, 
-                                        self.all_sprites)        
+                                        self.all_sprites)
+        self.viewport = util.SCREEN_RECT.copy()
 
     def make_walls(self):
         x = 0
         y = 0
         walls = pg.sprite.LayeredDirty()
-        for row in WORLD:
+        for row in util.WORLD:
             for col in row:
                 if col == "#":
                     Wall((x,y), walls, self.all_sprites)
-                x += WALL_SIZE
-            y += WALL_SIZE
+                x += util.WALL_SIZE
+            y += util.WALL_SIZE
             x = 0
 
         return walls
@@ -71,6 +61,7 @@ class Level(object):
         self.player_singleton.update(dt, keys, self.enemies, self.walls)
         self.enemies.update(dt, current_time, self.walls)
         util.gfx_group.update(dt)
+        self.update_viewport()
 
         for sprite in self.all_sprites:
             layer = self.all_sprites.get_layer_of_sprite(sprite)
@@ -82,19 +73,26 @@ class Level(object):
             for wall in hits:
                 wall.dirty = 1
 
+    def update_viewport(self):
+        self.viewport.center = self.player.rect.center
+        self.viewport.clamp_ip(self.rect)
+
     def render(self, surface):
-        rects1 = util.gfx_group.draw(surface)
-        rects2 = self.all_sprites.draw(surface)
-        return rects1 + rects2
+        util.gfx_group.clear(self.image, self.background)
+        self.all_sprites.clear(self.image, self.background)
+        util.gfx_group.draw(self.image)
+        self.all_sprites.draw(self.image)
+        dirty = surface.blit(self.image, (0,0), self.viewport)
+        return dirty
+
 
 
 class Wall(pg.sprite.DirtySprite):
     """docstring for Wall"""
     def __init__(self, pos, *gorups):
         super(Wall, self).__init__(*gorups)
-        self.image = pg.Surface((WALL_SIZE, WALL_SIZE))
+        self.image = pg.Surface((util.WALL_SIZE, util.WALL_SIZE))
         self.image.fill((155,255,155))
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
-        self.dirty = 1
-        
+        self.dirty = 1        
